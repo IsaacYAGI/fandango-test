9500f25e353d7a887681f32bc02f4836b01e7442
Isaac Yriarte <isaacyagi@gmail.com>
2020-02-26 23:33:41 -0500

    Test: Agrega mas test

--- exercise_one/test.js
+++ exercise_one/test.js
@@ -8,6 +8,14 @@ it('should return [2,8]', () => {
   output.should.be.eql([2,8]);
 });
 
-it('should return false', () => {
-    false.should.be.equal(false);
+it('should return [0,20]', () => {
+  let input = [45, 0, 9, 4, 20];
+  let output = calculator.subSetCalculator(input,20);
+  output.should.be.eql([0,20]);
+});
+
+it('should return [9,9]', () => {
+  let input = [1, 18, 9, 7, 9, 8, 10];
+  let output = calculator.subSetCalculator(input,18);
+  output.should.be.eql([9,9]);
 });
