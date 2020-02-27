daad18b71767b9aede4c12ca6ac67d921ee17ee4
Isaac Yriarte <isaacyagi@gmail.com>
2020-02-27 00:25:13 -0500

    Fix: Ajusta la logica para que llegue hasta el final del arreglo

--- exercise_one/utils.js
+++ exercise_one/utils.js
@@ -1,8 +1,7 @@
 const subSetCalculator = (M, N) => {
-  
-    for (let i = 0;i<M.length - 1;i++){
-        for (let j = i + 1; j < M.length - 1; j++){
-            if ((M[i] + M[j]) === N) return [M[i],M[j]];
+    for (let i = 0;i<M.length - 1; i++){
+        for (let j = i + 1; j < M.length; j++){
+            if ((+M[i] + +M[j]) === +N) return [M[i],M[j]];
         }
     }
     return [0,0];
