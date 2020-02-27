c4c5e1087a153a32fca0a5c31fd68d7975d28192
Isaac Yriarte <isaacyagi@gmail.com>
2020-02-26 23:45:09 -0500

    Feat: Desarrolla la implementacion

--- exercise_one/utils.js
+++ exercise_one/utils.js
@@ -1,6 +1,11 @@
 const subSetCalculator = (M, N) => {
-    //return N == 1;
-    return [2,8];
+  
+    for (let i = 0;i<M.length - 1;i++){
+        for (let j = i + 1; j < M.length - 1; j++){
+            if ((M[i] + M[j]) === N) return [M[i],M[j]];
+        }
+    }
+    return [0,0];
 }
 
 module.exports = { subSetCalculator }
