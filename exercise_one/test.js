e07cc89507aa1f8a6172e7696f82cbdb9b73fe73
Isaac Yriarte <isaacyagi@gmail.com>
2020-03-07 21:06:37 -0500

    Test: Agrega mas pruebas unitarias

--- exercise_one/test.js
+++ exercise_one/test.js
@@ -24,3 +24,21 @@ it('subsetCalculator should return [8,10]', () => {
   let output = calculator.subSetCalculator(input,18);
   output.should.be.eql([8,10]);
 });
+
+it('subsetCalculator should return [2,3]', () => {
+  let input = [2,1,9,5,3];
+  let output = calculator.subSetCalculator(input,5);
+  output.should.be.eql([2,3]);
+});
+
+it('subsetCalculator should return [37,0]', () => {
+  let input = [21,31,49,55,37,0,10,15,100];
+  let output = calculator.subSetCalculator(input,37);
+  output.should.be.eql([37,0]);
+});
+
+it('subsetCalculator should return [1,4]', () => {
+  let input = [1,2,4,1,4,2,3,4,3,2,1];
+  let output = calculator.subSetCalculator(input,5);
+  output.should.be.eql([1,4]);
+});
