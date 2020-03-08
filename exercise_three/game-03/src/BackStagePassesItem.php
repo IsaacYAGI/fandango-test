f9e1665ce6dc25c5471e4df788ac0537efeaf695
Isaac Yriarte <isaacyagi@gmail.com>
2020-03-07 20:00:05 -0500

    Refactor: Agrega la logica de BackStageItem y descomenta los test de BackStage passes

--- exercise_three/game-03/src/BackStagePassesItem.php
+++ exercise_three/game-03/src/BackStagePassesItem.php
@@ -14,6 +14,26 @@ class BackStagePassesItem extends Item
     public function updateData()
     {
         $this->decreaseSellIn();
-        $this->decreaseQuality(1);
+
+        //Si ya paso el concierto, la calidad se coloca en 0 automaticamente
+        if ($this->getSellIn() < 0){
+            $this->setQuality(0);
+        }else{ // Sino se obtiene el factor y se aplica el incremento en calidad correspondiente
+            $this->increaseQuality($this->getQualityFactor());
+        }
+    }
+
+    protected function getQualityFactor()
+    {
+        $sellIn = $this->getSellIn();
+
+        //Si quedan menos de 5 dias se aumenta en 3 la calidad
+        if ($sellIn < 5) return 3;
+
+        //Si quedan menos de 10 dias se aumenta en 2 la calidad
+        if ($sellIn < 10) return 2;
+
+        //En cualquier otro caso se usa el valor estandar
+        return 1;
     }
 }
